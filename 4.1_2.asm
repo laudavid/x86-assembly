fcc6c1ecf5c24685e5955535e641f58a8b5a11a7
David Lau <liu1351853931@outlook.com>
2019-05-06 02:57:58 +0800

    Add 4.1.asm

---
+++ 4.1_2.asm
@@ -0,0 +1,120 @@
+;多次查找并删除指定字符，只有第一次输出结果
+STACK 	SEGMENT	PARA	STACK
+		DW		100H DUP(?)
+STACK	ENDS
+
+DATA	SEGMENT	PARA
+	S1				DB	'Hail Hydra! Hail Hydra!',00H,'$'
+	CHAR			DB	'l'
+	MSG_IN			DB	' in ','$'
+	MSG_NOT_FOUND	DB	'Not found ','$'
+    NEW_LINE 		DB	0DH,0AH,'$'
+DATA 	ENDS
+
+CODE 	SEGMENT PARA
+		ASSUME	CS:CODE,DS:DATA,SS:STACK
+
+
+PUT_CHAR 		MACRO	CHAR
+		PUSH  	DX
+		PUSH 	AX
+        
+		MOV     DL,CHAR
+        MOV     AH,2
+        INT     21H
+		
+		POP 	AX
+		POP	 	DX
+ENDM
+
+
+DISP_MSG		MACRO	MSG
+		PUSH  	DX
+		PUSH 	AX
+		
+		MOV     DX,OFFSET MSG
+		MOV     AH,9
+		INT     21H
+		
+		POP 	AX
+		POP	 	DX
+ENDM
+
+
+Find_ch PROC
+		PUSH 	BP
+		MOV	 	BP,SP
+		PUSH	DI
+		PUSH	SI
+		PUSH	AX
+		PUSH	CX
+
+		XOR		CX,CX
+
+FIND_START:
+		MOV 	DI,[BP+6]
+		MOV 	AX,[BP+4]
+
+        CLD
+SC_NEXT:
+		SCASB
+		JZ 		FOUND
+		CMP     BYTE PTR [DI],0
+		JNZ		SC_NEXT
+
+		CMP		CX,0
+		JNZ		FIND_RET
+
+		DISP_MSG	MSG_NOT_FOUND
+		PUT_CHAR	CHAR
+		DISP_MSG	MSG_IN
+		DISP_MSG	S1
+		DISP_MSG	NEW_LINE
+		JMP			FIND_RET
+FOUND:
+		CMP		CX,0
+		JNZ		DELETE
+		
+		PUT_CHAR	CHAR
+		DISP_MSG	MSG_IN
+		DISP_MSG	S1
+		DISP_MSG	NEW_LINE
+
+DELETE:
+		INC		CX
+		MOV		SI,DI
+		DEC		DI
+MOV_NEXT:
+		MOVSB
+		CMP		BYTE PTR [DI],'$'
+		JNZ		MOV_NEXT
+
+		JMP		FIND_START
+
+
+FIND_RET:
+		POP		AX
+		POP		SI
+		POP		DI
+		POP 	BP
+		RET		4
+Find_ch ENDP
+
+MAIN	PROC
+		MOV 	AX,DATA
+		MOV 	DS,AX
+		MOV 	ES,AX
+
+		MOV		DX,OFFSET S1
+		PUSH	DX
+		MOV		DL,CHAR
+		XOR		DH,DH
+		PUSH	DX
+		CALL	Find_ch
+
+		MOV 	AX,4C00H
+		INT 	21H
+MAIN	ENDP
+
+CODE	ENDS
+		END		MAIN
